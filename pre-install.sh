ed3e19f6078382d644ea8d4196a938572c11d328
Spencer <75862693+TacoMonkey11@users.noreply.github.com>
2022-05-02 20:14:45 -0400

    Refractoring

--- pre-install.sh
+++ pre-install.sh
@@ -1,9 +1,12 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 normal=$'\e[0m'                           
 bold=$(tput bold)                                      
 brown=$(tput setaf 94)                
 green=$(tput setaf 2)                   
 tag="${green}[${brown}MSI${green}]${normal}"
+
+checks=0
+declare -i checks
 
 checkJava() {
     javaVersion=$1
@@ -16,6 +19,7 @@ checkJava() {
         if ls /usr/lib/jvm | grep -q $javaVersion
         then 
             echo "${tag} Java ${javaVersion} has been detected"
+            checks+=1
         else 
             echo "${tag} Java ${javaVersion} has NOT been detected, it may not be installed"
         fi
@@ -23,6 +27,7 @@ checkJava() {
         if /usr/libexec/java_home -v $javaVersion >/dev/null
         then 
             echo "${tag} Java ${javaVersion} has been detected"
+            checks+=1
         else 
             echo "${tag} Java ${javaVersion} has NOT been detected, it may not be installed"
         fi
@@ -33,6 +38,7 @@ checkTmux() {
     if command -v tmux >/dev/null
     then
         echo "${tag} Tmux has been detected"
+        checks+=1
     else 
         echo "${tag} Tmux has not been detected"
     fi
@@ -45,4 +51,20 @@ fi
 checkJava "1.8" && checkJava "17"
 checkTmux
 
+if [[ ${checks} == 3 ]] 
+then 
+    echo "${tag} Passed all checks. Moving on.\n"
+else 
+    echo "${tag} Some checks have failed, exiting\n"
+    return 0
+    exit
+fi
 
+if [[ "$SHELL" == "/bin/bash" ]] 
+then
+    echo "${tag} Add the following to your .bashrc:"
+elif [[ "$SHELL" == "/bin/zsh" ]]
+then
+    echo "${tag} Add the following to your .zshrc:"
+fi
+echo "export PATH=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )/script:\$PATH"
